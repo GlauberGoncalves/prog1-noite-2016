fdb6f082426d25b1d6c3fc6a573350667238cc64
Glauber <glauber_g3@yahoo.com.br>
2016-05-01 23:24:09 +0000

    v01051620

---
+++ vetor1.c
@@ -0,0 +1,17 @@
+// Vetor int
+
+#include <stdio.h>
+
+int main()
+{
+    int i,vetor[31];
+    
+    for(i=1;i<=30;i++)
+       {
+           vetor[i] = i * 2;
+           printf("Vetor[%d] == %d \n",i,vetor[i]);
+       }   
+    
+
+
+}
