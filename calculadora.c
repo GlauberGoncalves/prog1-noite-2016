155e77b4e97d675376bd0c72881a86954fa21f33
Glauber <glauber_g3@yahoo.com.br>
2016-05-09 20:20:41 +0000

    calculadora aprimorada - level 1

--- calculadora.c
+++ calculadora.c
@@ -1,107 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-float soma(float num1 , float num2)
+
+/*
+     Calculadora em c
+*/
+
+float calcular(float a, float b, char c)
 {
-    float resultado;
-    resultado = num1 + num2;
-    printf("= %f\n", resultado);
+	float resultado;
+
+	if ( c == '+' )
+	resultado = a + b;
+	else
+	if ( c == '-' )
+	resultado = a - b;
+	else
+	if ( c == '*' )
+	resultado = a * b;	
+	else
+	if ( c == '/' )
+	resultado = a / b;
+ return resultado;	
 }
 
-float subtracao(float num1 , float num2)
+char recebe_dados( char visor[], char operacao)
 {
-    float resultado;
-    resultado = num1 - num2;
-    printf("= %f\n", resultado);
-}
+	char digitado;
+	
+	digitado = getchar;
+	if (digitado == '+' || digitado == '-' || digitado == '*' || digitado == '/')
+		operacao = digitado;
+	else
+		return digitado;
 
-float multiplicacao(float num1 , float num2)
-{
-    float resultado;
-    resultado = num1 * num2;
-    printf("= %f\n", resultado);
-}
 
-float divisao(float num1 , float num2)
-{
-    float resultado;
-    resultado = num1 / num2;
-    printf("= %f\n", resultado);
 }
 
 int main()
 {
-    int opcao;
-    float num1, num2;
+	char a;
+	float resultado;
+	
+	
+	printf("%f\n\n", resultado);
+	
+	resultado = calcular(4, 5, '*');
+	
+	printf("%f\n\n", resultado);
 
-
-do{
-    printf("\nEscolha a opção\n\n");
-    printf("1 - adição\n");
-    printf("2 - subtracao\n");
-    printf("3 - multiplicação\n");
-    printf("4 - divisão\n");
-    scanf("%d",&opcao);
-    
-    
-    switch (opcao)
-    {
-    case 1: // soma
-        {
-            printf("Entre com num1:");
-            scanf("%f",&num1);
-            
-            printf("Entre com num1:");
-            scanf("%f",&num2);
-            
-            soma(num1,num2);
-            break;
-        }
-    case 2:
-        {
-            printf("Entre com num1:");
-            scanf("%f",&num1);
-            
-            printf("Entre com num1:");
-            scanf("%f",&num2);
-            
-            subtracao(num1,num2);
-            break;
-        }    
-    case 3: 
-        {
-            printf("Entre com num1:");
-            scanf("%f",&num1);
-            
-            printf("Entre com num1:");
-            scanf("%f",&num2);
-            
-            multiplicacao(num1,num2);
-            break;
-        }   
-    case 4: 
-    
-        {
-            printf("Entre com num1:");
-            scanf("%f",&num1);
-            
-            printf("Entre com num1:");
-            scanf("%f",&num2);
-            
-            divisao(num1,num2);
-            break;
-        } 
-       default:{
-           
-                printf("Opção incorreta. Digite novamente");
-                break;
-              }
-       }
-    printf("Deseja continuar 1-sim | 2 - não \n");
-    scanf ("%d", &opcao);
-    system("clear");
-    
- }while(opcao != 2);
-       
-       
-    return 0;   
+	
 }
