95ed88b0461dd9b31893705105dcef86dc9b3f46
Glauber <glauber_g3@yahoo.com.br>
2016-05-10 19:36:06 +0000

    melhora da função recebe dados

--- calculadora.c
+++ calculadora.c
@@ -1,53 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /*
      Calculadora em c
 */
 
-float calcular(float a, float b, char c)
+float calcular(char num1[], char num2[], char operacao)
 {
-	float resultado;
+	float numero1, numero2, resultado;
+	
+	numero1 = atof(num1);
+	numero1 = atof(num2);
 
-	if ( c == '+' )
-	resultado = a + b;
+	if ( operacao == '+' )
+	resultado = numero1 + numero2;
 	else
-	if ( c == '-' )
-	resultado = a - b;
+	if ( operacao == '-' )
+	resultado = numero1 - numero2;
 	else
-	if ( c == '*' )
-	resultado = a * b;	
+	if ( operacao == '*' )
+	resultado = numero1 * numero2;	
 	else
-	if ( c == '/' )
-	resultado = a / b;
+	if ( operacao == '/' )
+	resultado = numero1 / numero2;
  return resultado;	
+
+//	resultado = calcular(4, 5, '*'); já testado
+
 }
 
-char recebe_dados( char visor[], char operacao)
+void recebe_dados( char numero[], char operacao, int muda_numero)
 {
-	char digitado;
+	char digitado, cont;
+ do
+ {
 	
-	digitado = getchar;
+	digitado = getchar();
+	
 	if (digitado == '+' || digitado == '-' || digitado == '*' || digitado == '/')
+	{
 		operacao = digitado;
-	else
-		return digitado;
-
-
+		muda_numero = 1;
+		printf("\n%c\n", operacao);
+	}	
+	else if (digitado == 13)
+	{
+		
+		
+	}
+	else 
+	{
+		numero[cont] = digitado;
+		cont++;
+		system("clear");
+		printf("%c", numero[cont]);
+	}
+  }while(1);
+	
 }
 
 int main()
 {
-	char a;
-	float resultado;
-	
-	
-	printf("%f\n\n", resultado);
-	
-	resultado = calcular(4, 5, '*');
-	
-	printf("%f\n\n", resultado);
+	char char_numero[12], operacao;
+	float numero[20], resultado;
+	int muda_numero, cont;
 
-	
+	cont = 0;
+
+// inicio do programa	
+// Recebe o primeiro numero digitado pelo usuario ea operação
+/*	do{
+		recebe_dados( char_numero[cont], operacao, muda_numero);
+		cont++;
+		printf("%c", char_numero);
+	}while(muda_numero == 0 || cont == 10);
+*/	
+
+recebe_dados(char_numero, operacao, muda_numero);
+
 }
